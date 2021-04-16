2835ac3d40f0e1e300840d53fac0ac3a1d5080e0
adriel <adriel_sanmillan@yahoo.com>
2021-04-17 02:22:47 +0800

    1.0

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("The Game") 
+console.log("The Game")
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0px";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollPos = currentScrollPos;
+}
